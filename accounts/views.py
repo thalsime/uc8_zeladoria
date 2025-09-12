cce30885b2d073ad26f57864cf779db3e27b15e6
thalsime <alunos.senac@tsr.net.br>
2025-09-12 11:34:04 -0300

    fix: bug do IsAdmin / IsStaff

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,8 @@ from rest_framework import viewsets, status, permissions, generics, parsers
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from core.permissions import IsAdminUser
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework import generics
 from django.contrib.auth.models import Group, User
